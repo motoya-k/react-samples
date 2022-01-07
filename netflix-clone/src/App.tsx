b58433a92614f28b295fde327f9adc7b6a3e0a59
motoyannn <motoya.kon@gmail.com>
2022-01-07 19:04:38 +0900

    feat: add several sections

--- netflix-clone/src/App.tsx
+++ netflix-clone/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Row, Banner , GlobalNav} from "src/components";
-
 import { MOVIEAPI } from "src/plugins/apiConsts";
+import './App.css'
 
 const App: React.FC = () => {
   return (
@@ -11,6 +11,46 @@ const App: React.FC = () => {
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={MOVIEAPI.fetchNetflixOriginals}
+        size="large"
+      />
+      <Row
+        title="TOP RATED"
+        fetchUrl={MOVIEAPI.fetchTopRated}
+        size="large"
+      />
+      <Row
+        title="TRENDING"
+        fetchUrl={MOVIEAPI.fetchTrending}
+        size="large"
+      />
+      <Row
+        title="ACTION MOVIES"
+        fetchUrl={MOVIEAPI.fetchActionMovies
+        }
+        size="large"
+      />
+      <Row
+        title="COMEDY MOVIES"
+        fetchUrl={MOVIEAPI.fetchComedyMovies
+        }
+        size="large"
+      />
+      <Row
+        title="HORROR MOVIES"
+        fetchUrl={MOVIEAPI.fetchHorrorMovies
+        }
+        size="large"
+      />
+      <Row
+        title="ROMANCE MOVIES"
+        fetchUrl={MOVIEAPI.fetchRomanceMovies
+        }
+        size="large"
+      />
+      <Row
+        title="DOCUMENT MOVIES"
+        fetchUrl={MOVIEAPI.fetchDocumentMovies
+        }
         size="large"
       />
     </div>
